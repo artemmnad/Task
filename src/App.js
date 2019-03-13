43500f63088401b46ce1a10c1c9fd05a8803ad1b
Arutyun Mnatsakanyan <artemmnad@gmail.com>
2019-03-13 13:14:28 +0300

    Start integrating redux into project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import UsersScreen from './screens/UsersScreen';
 import {
@@ -9,22 +10,23 @@ import {
 } from 'antd';
 const { Content, Header } = Layout;
 
-
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Layout>
-          <Header>
-            <Select />
-            <DatePicker />
-          </Header>
-          <Content>
-            <Route exact path="/" render={() => (<Redirect to="/users"/>)} />
-            <Route path="/users" component={UsersScreen} />
-          </Content>
-        </Layout>
-      </Router>
+      <Provider store={this.props.store}>
+        <Router>
+          <Layout>
+            <Header>
+              <Select />
+              <DatePicker />
+            </Header>
+            <Content>
+              <Route exact path="/" render={() => (<Redirect to="/users"/>)} />
+              <Route path="/users" component={UsersScreen} />
+            </Content>
+          </Layout>
+        </Router>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
